d88e08caf7b91e9f67e6bd7aafb22ca00e7f9bd8
skalshetty57 <74254221+skalshetty57@users.noreply.github.com>
2021-08-18 13:09:26 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
 
+import React, {useState} from "react";
+
+function Selection(props) {
+  const [stateVar,setStateVar]=useState({background:''});
+
+  let handleClick =() =>{
+    props.applyColor(setStateVar);
+  }
+
+  return <div 
+          className="fix-box"
+          style={stateVar}
+          onClick={handleClick}
+          >
+            <h2 className="subheading">Selection</h2>
+          </div>;
+}
+export default Selection;
